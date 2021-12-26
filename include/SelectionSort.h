51f58088274c2359c850924ef9a95a1da23553ab
mynameisissac <kaikaijjj123@gmail.com>
2021-12-26 23:46:26 +0800

    SelectionSort members declaration

---
+++ include/SelectionSort.h
@@ -0,0 +1,25 @@
+//
+// Created by kshou on 2021/12/26.
+//
+
+#ifndef SORTINGVISUALIZER_SELECTIONSORT_H
+#define SORTINGVISUALIZER_SELECTIONSORT_H
+
+#include "SortingAlgorithm.h"
+
+class SelectionSort : public SortingAlgorithm{
+    private:
+        // indicate which iteration is currently sort to
+        int stage;
+
+    public:
+        explicit SelectionSort(Array<int>& arrayToSort);
+
+        static int findMin(int* arr, int size);
+
+        void sortOneIteration() override;
+
+};
+
+
+#endif //SORTINGVISUALIZER_SELECTIONSORT_H
---
+++ src/SelectionSort.cpp
@@ -0,0 +1,24 @@
+//
+// Created by kshou on 2021/12/26.
+//
+
+#include "SelectionSort.h"
+
+
+SelectionSort::SelectionSort(Array<int>& arrayToSort)
+    : SortingAlgorithm(arrayToSort), stage(0)
+{}
+
+
+// static memeber function
+int SelectionSort::findMin(int* arr, int size)
+{
+
+}
+
+
+void SelectionSort::sortOneIteration()
+{
+
+}
+
